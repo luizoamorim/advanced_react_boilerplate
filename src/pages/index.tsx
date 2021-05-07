b8401779bf05c3f40c91877773c2b567d1e4d48f
Luiz <luiz.oamorim@gmail.com>
2021-05-07 20:49:54 -0300

    environment setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  let bla = 'teste'
+
   return (
     <div className={styles.container}>
       <Head>
