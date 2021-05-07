3ac4df30437a0b0ad6f235ac36d3a75e69edc82e
Luiz <luiz.oamorim@gmail.com>
2021-05-07 20:51:38 -0300

    test husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-
-  let bla = 'teste'
+  const bla = 'teste'
 
   return (
     <div className={styles.container}>
